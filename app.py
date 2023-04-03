7170e1733b7609b00b2ab3ac4389e045c1bd1dc9
Mahyar Osanlouy <mahyar.osn@gmail.com>
2023-04-03 17:40:32 +1000

    add more interactive plotting, saving sessions, more content

--- app.py
+++ app.py
@@ -14,11 +14,10 @@ from src.np_implementation.model import TPC
 from src.np_implementation.data import generate_random_nonlinear_data
 
 # Set up the page configuration
-st.set_page_config(page_title='Temporal Predictive Coding', layout='wide', initial_sidebar_state='collapsed',
+st.set_page_config(page_title='Temporal Predictive Coding', layout='centered', initial_sidebar_state='collapsed',
                    page_icon=None)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-
 
 if "button1_clicked" not in st.session_state:
     st.session_state.button1_clicked = False
@@ -64,7 +63,8 @@ def run_simulation_random_data_np(activation, timepoints):
             (-1., -dt / 2)))) * 2.5
 
         # Generate random data (add your generate_random_nonlinear_data function)
-        y_truth, A_truth = generate_random_nonlinear_data(2, 2, timepoints, x, C_init, A_init, seed=random.randint(0, 200))
+        y_truth, A_truth = generate_random_nonlinear_data(2, 2, timepoints, x, C_init, A_init,
+                                                          seed=random.randint(0, 200))
 
         # Infer the data with temporal predictive coding (add your TPC class)
         A = np.zeros((y_truth.shape[0], y_truth.shape[0]))
@@ -228,7 +228,6 @@ def run_tracking_learning(input_weights):
 
             # learn A C
             if input_weights == "Learn":
-
                 AC_nkf = NeuralKalmanFilter(init_A, B, init_C, latent_size=3).to(device)
                 AC_nkf.train(xs, us, inf_iters, inf_lr, learn_iters, learn_lr)
                 zs_nkf, xs_nkf = AC_nkf.predict(xs, us, inf_iters, inf_lr)
@@ -323,56 +322,139 @@ def main():
             template="plotly_dark",
         )
         st.plotly_chart(fig)
-    with plot_columns[1]:
-        st.markdown(r"This interactive figure demonstrates the estimates of the acceleration with different models."
-                    r"Specifically we compare different step sizes of inference between observations with Kalman"
-                    r"Filter. As you toggle between different step size, you can see that the performance of the "
-                    r"temporal predictive coding model significantly improves with larger steps and it becomes "
-                    r"comparable to that of Kalman Filter. The tPC with single inference step has the worst "
-                    r"performance; however, it still achieves a smooth fit to the system state")
+    # with plot_columns[1]:
+    st.markdown(r"This interactive figure demonstrates the estimates of the acceleration with different models."
+                r"Specifically we compare different step sizes of inference between observations with Kalman "
+                r"Filter. As you toggle between different step size, you can see that the performance of the "
+                r"temporal predictive coding model significantly improves with larger steps and it becomes "
+                r"comparable to that of Kalman Filter. The tPC with single inference step has the worst "
+                r"performance; however, it still achieves a smooth fit to the system state")
 
     st.header("Effect of learning A & C matrices")
+    st.markdown(r"""In above example, we only inferred the hidden states of the world by assuming known values for
+     $A$ and $C$. However, in many real-world situations, we cannot make this assumption, as the structure of the 
+     dynamics or observation functions may be unknown. Instead, we need to learn these matrices from observations.
+     Our recurrent predictive coding model incorporates a Hebbian plasticity-based learning rule to learn these 
+     matrices directly. We explore the impact of learning these parameters on the model's performance by examining 
+     three different methods for setting $A$ and $C$ values:
+     
+ 1. using the true values from the data generation; 
+ 2. learning them via our model's learning rule; and
+ 3. setting them to random values. 
+     
+ We evaluate the performance of these models on both latent state level ($x$) and observation level ($y$), 
+ assessing the accuracy of the model's estimates for each level. 
+ It's important to note that the observation estimates are calculated as the projected latent estimate by $C$, 
+ that is, $\hat{y}_k = C\hat{x}_k$, where the value of $C$ is obtained at each time step $k$.
+ 
+ Check out the effect of different methods mentioned using the plotting tool below:
+    """)
+
+
     ac_columns = st.columns(2)
     ac_plot_columns = st.columns(2)
+    col1, col2 = st.columns(2)
     with ac_columns[0]:
         init_w = st.selectbox("Initial weights", ["True", "Learn", "Random"])
     with ac_columns[0]:
         if st.button("Run experiment"):
-            st.session_state.button1_clicked = True
-
-        if st.session_state.button1_clicked:
+            # st.session_state.button1_clicked = True
+            # if st.session_state.button1_clicked:
             zs_kf1, xs_kf1, zs_nkf1, xs_nkf1, zs1, xs1 = run_tracking_learning(init_w)
-            with ac_plot_columns[0]:
-                fig = go.Figure()
-                fig.add_trace(go.Scatter(y=zs1[0], mode="lines", name=f"True Value"))
-                fig.add_trace(go.Scatter(y=zs_nkf1[0], mode="lines", name=f"{init_w}"))
-                fig.add_trace(go.Scatter(y=zs_kf1[0], mode="lines", name=f"Kalman Filter)"))
-                fig.update_layout(
-                    title=f"State",
-                    xaxis_title="Time",
-                    yaxis_title=r'$x_1$',
-                    template="plotly_dark",
+            fig1 = go.Figure()
+            fig1.add_trace(go.Scatter(y=zs1[0], mode="lines", name=f"True Value"))
+            fig1.add_trace(go.Scatter(y=zs_nkf1[0], mode="lines", name=f"{init_w} A & C"))
+            fig1.add_trace(go.Scatter(y=zs_kf1[0], mode="lines", name=f"Kalman Filter"))
+            fig1.update_layout(
+                title=f"State",
+                xaxis_title="Time",
+                yaxis_title=r'$x_1$',
+                template="plotly_dark",
+                legend=dict(
+                    x=1.05,  # X position (fraction) relative to the right of the plot
+                    y=1,  # Y position (fraction) relative to the top of the plot
+                    xanchor='right',  # Anchor the legend's left side
+                    yanchor='bottom',  # Anchor the legend's top side
                 )
-                st.plotly_chart(fig)
 
-            with ac_plot_columns[1]:
-                fig = go.Figure()
-                fig.add_trace(go.Scatter(y=xs1[0], mode="lines", name=f"True Value"))
-                fig.add_trace(go.Scatter(y=xs_nkf1[0], mode="lines", name=f"{init_w}"))
-                fig.add_trace(go.Scatter(y=xs_kf1[0], mode="lines", name=f"Kalman Filter"))
-                fig.update_layout(
-                    title=f"Observed",
-                    xaxis_title="Time",
-                    yaxis_title=r'$y_1$',
-                    template="plotly_dark",
+            )
+            # st.plotly_chart(fig)
+
+            # with col2:
+            fig2 = go.Figure()
+            fig2.add_trace(go.Scatter(y=xs1[0], mode="lines", name=f"True Value"))
+            fig2.add_trace(go.Scatter(y=xs_nkf1[0], mode="lines", name=f"{init_w} A & C"))
+            fig2.add_trace(go.Scatter(y=xs_kf1[0], mode="lines", name=f"Kalman Filter"))
+            fig2.update_layout(
+                title=f"Observed",
+                xaxis_title="Time",
+                yaxis_title=r'$y_1$',
+                template="plotly_dark",
+                legend=dict(
+                    x=1.05,  # X position (fraction) relative to the right of the plot
+                    y=1,  # Y position (fraction) relative to the top of the plot
+                    xanchor='right',  # Anchor the legend's left side
+                    yanchor='bottom',  # Anchor the legend's top side
                 )
-                st.plotly_chart(fig)
 
+            )
+            # st.plotly_chart(fig)
+
+            col1.plotly_chart(fig1, use_container_width=True)
+            col2.plotly_chart(fig2, use_container_width=True)
 
     ### Nonlinear stuff
     st.header("Comparison between linear and non-linear models")
-    st.markdown(r"..............................")
-    st.markdown(r"..............................")
+    st.markdown(r"""
+    We explored an adjusted version of the temporal predictive coding model that incorporates nonlinear functions 
+    $f$ and $g$ in the prediction error equations. This results in simpler parameter update rules, making it easier
+     for neural implementation:
+    $$
+    \epsilon_y = y_k - Cf(\hat{x}_k)
+    $$
+    $$
+    \epsilon_x = \hat{x}_k - Ag(\hat{x}_{k-1})
+    $$
+    $$
+    \frac{d \hat{x}_k}{dt} = - \frac{\partial \mathcal{F}}{\partial \hat{x}_k} = f'(\hat{x}_k) \odot C^T  \epsilon_y - \epsilon_x
+    $$
+    $$
+    \Delta C = - \eta\frac{\partial \mathcal{F}}{\partial C} = \eta\epsilon_y f(\hat{x}_k^T)
+    $$
+    $$
+    \Delta A = - \eta\frac{\partial \mathcal{F}}{\partial A} = \eta\epsilon_x g(\hat{x}_{k-1}^T)
+    $$
+    
+
+    We tested the model in two experiments: 1) simulating random synthetic data and inferring immediate future 
+    observations, and 2) simulating pendulum swinging dynamics and testing learning capabilities.
+
+    """)
+
+    st.subheader("Experiment 1: Synthetic non-linear data")
+    st.markdown(r"""
+    For the synthetic stimuli experiment, we can generate a two-dimensional data using the following
+    nonlinear equations:
+
+    $$
+    x_k = A g(x_{k-1}) + \omega_x
+    $$
+    $$
+    y_k = C f(x_k) + \omega_y
+    $$
+    
+    where the nonlinear functions $f$ and $g$ are both chosen to be the hyperbolic tangent function, 
+    and $\omega_y$ and $\omega_x$ denote standard Gaussian noise sampled from an $i.i.d$ Gaussian distribution 
+    with a mean of 0 and a variance of 0.01. In this 2D example, parameters of $C$ and $A$ were set to a 
+    rotation matrix and the identity matrix.
+    
+    Use the following interactive tool to run a simulation. "Activation" refers to the non-linearity activation function
+    to use in the simulation. The non-linear function is a hyperbolic tan.
+    
+    Note that each time that you run the simulation, a newly random data is generated. 
+    
+    """)
+
     # Create columns for the input fields
     input_columns = st.columns(2)
 
@@ -389,39 +471,48 @@ def main():
 
     # Create the "Run simulation" button
     if st.button("Run simulation"):
-        st.session_state.button2_clicked = True
-
-    if st.session_state.button2_clicked:
+        # st.session_state.button2_clicked = True
+        # if st.session_state.button2_clicked:
         solution, error = run_simulation_random_data_np(activation, timepoints)
 
         # Create columns for the plots
         plot_columns = st.columns(2)
+        col3, col4 = st.columns(2)
 
         # Plot the y_truth and predicted values in the first column
-        with plot_columns[0]:
-            fig = go.Figure()
-            fig.add_trace(go.Scatter(y=solution[:, 0].T, mode="lines", name="True"))
-            fig.add_trace(go.Scatter(y=solution[:, 1].T, mode="lines", name=f"Estimated ({activation})"))
-            fig.update_layout(
-                title=f"True state vs. estimated ({activation})",
-                xaxis_title="Time",
-                yaxis_title="Magnitude",
-                template="plotly_dark",
+        # with plot_columns[0]:
+        fig3 = go.Figure()
+        fig3.add_trace(go.Scatter(y=solution[:, 0].T, mode="lines", name="True"))
+        fig3.add_trace(go.Scatter(y=solution[:, 1].T, mode="lines", name=f"Estimated ({activation})"))
+        fig3.update_layout(
+            title=f"True state vs. estimated ({activation})",
+            xaxis_title="Time",
+            yaxis_title="Magnitude",
+            template="plotly_dark",
+            legend=dict(
+                x=1.05,  # X position (fraction) relative to the right of the plot
+                y=1,  # Y position (fraction) relative to the top of the plot
+                xanchor='right',  # Anchor the legend's left side
+                yanchor='bottom',  # Anchor the legend's top side
             )
-            st.plotly_chart(fig)
+        )
+        # st.plotly_chart(fig)
 
         # Plot the error in the second column
         moving = 100
-        with plot_columns[1]:
-            fig = go.Figure()
-            fig.add_trace(go.Scatter(y=__moving_average(error, moving).T, mode="lines", name="Error"))
-            fig.update_layout(
-                title="Mean squared error",
-                xaxis_title="Time",
-                yaxis_title="Error",
-                template="plotly_dark",
-            )
-            st.plotly_chart(fig)
+        # with plot_columns[1]:
+        fig4 = go.Figure()
+        fig4.add_trace(go.Scatter(y=__moving_average(error, moving).T, mode="lines", name="Error"))
+        fig4.update_layout(
+            title="Mean squared error",
+            xaxis_title="Time",
+            yaxis_title="Error",
+            template="plotly_dark",
+        )
+        # st.plotly_chart(fig)
+
+        col3.plotly_chart(fig3, use_container_width=True)
+        col4.plotly_chart(fig4, use_container_width=True)
 
 
 if __name__ == '__main__':
